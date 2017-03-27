402caf04c064f2978af96ce923116fcd58f695b5
Joscha Drechsler <drechsler@cs.tu-darmstadt.de>
2017-03-27 15:32:37 +0200

    almost almost working multitier version

--- Examples/examples/src/main/scala/examples/demo/QNetworkRacketBall.scala
+++ Examples/examples/src/main/scala/examples/demo/QNetworkRacketBall.scala
@@ -6,6 +6,7 @@ import examples.demo.ORacketMultiBall.Racket
 import examples.demo.PSplitscreenRacketBall.Opponent
 import examples.demo.ui._
 import rescala._
+import rescala.parrp.ParRP
 import retier._
 import retier.architectures.ClientServer._
 import retier.rescalaTransmitter._
@@ -25,21 +26,17 @@ object Transmittables {
     def send(value: Color, remote: RemoteRef) = (value.getRed, value.getGreen, value.getBlue, value.getAlpha)
     def receive(value: (Int, Int, Int, Int), remote: RemoteRef) = new Color(value._1, value._2, value._3, value._4)
   }
-  type Repr = (Signal[Int], Signal[Int], Signal[Int], Option[Signal[Int]], Signal[Option[Color]], Signal[Option[Color]])
+
+  type Repr = (Signal[Int], Signal[Int], Signal[Int], Option[Signal[Int]])
   implicit def shapeTransmittable[Inter](implicit transmittable: Transmittable[Repr, Inter, Repr]) = new PullBasedTransmittable[Shape, Inter, Shape] {
     def send(value: Shape, remote: RemoteRef) = transmittable send (value match {
-      case shape: Circle =>
-        (shape.centerX, shape.centerY, shape.diameter, None, shape.border, shape.fill)
-      case shape: Rectangle =>
-        (shape.centerX, shape.centerY, shape.hitboxWidth, Some(shape.hitboxHeight), shape.border, shape.fill)
+      case shape: Circle => (shape.centerX, shape.centerY, shape.diameter, None)
+      case shape: Rectangle => (shape.centerX, shape.centerY, shape.hitboxWidth, Some(shape.hitboxHeight))
     })
-    def receive(value: Inter, remote: RemoteRef) =
-      (transmittable receive value) match {
-        case (centerX, centerY, diameter, None, border, fill) =>
-          new Circle(centerX, centerY, diameter, border, fill)
-        case (centerX, centerY, hitboxWidth, Some(hitboxHeight), border, fill) =>
-          new Rectangle(centerX, centerY, hitboxWidth, hitboxHeight, border, fill)
-      }
+    def receive(value: Inter, remote: RemoteRef) = (transmittable receive value) match {
+      case (centerX, centerY, diameter, None) => new Circle(centerX, centerY, diameter)
+      case (centerX, centerY, hitboxWidth, Some(hitboxHeight)) => new Rectangle(centerX, centerY, hitboxWidth, hitboxHeight)
+    }
   }
 }
 import Transmittables._
@@ -50,11 +47,11 @@ object QNetworkRacketBall {
   trait Client extends ClientPeer[Server]
 
   val shapes = placed[Server] { implicit! => Var[List[Shape]](List.empty) }
-  val panel = placed[Server].local { implicit! => new ShapesPanel(shapes) }
-  val panelSize = placed[Server] { implicit! => panel.sigSize }
+  val panelX = placed[Server].local { implicit! => new ShapesPanel(shapes) }
+  val panelSize = placed[Server] { implicit! => panelX.sigSize }
 
-  val playingField = placed[Server].local { implicit! => new PlayingField(panel.width.map(_ - 25), panel.height.map(_ - 25)) }
-  val racket = placed[Server].local { implicit! => new Racket(playingField.width, true, playingField.height, panel.Mouse.y) }
+  val playingField = placed[Server].local { implicit! => new PlayingField(panelX.width.map(_ - 25), panelX.height.map(_ - 25)) }
+  val racket = placed[Server].local { implicit! => new Racket(playingField.width, true, playingField.height, panelX.Mouse.y) }
   placed[Server].local { implicit ! => shapes.transform(playingField.shape :: racket.shape :: _) }
 
   val opponentInput = placed[Client] { implicit! =>
@@ -63,18 +60,19 @@ object QNetworkRacketBall {
     opponent.panel2.Mouse.y
   }
   val racket2 = placed[Server].local { implicit! =>
-    new Racket(playingField.width, false, playingField.height, opponentInput.asLocal /* Signal {
-      remote[Client].connected() match {
-        case opponent :: _ => (opponentInput from opponent).asLocal()
-        case _ => 0
-      }
-    }*/)
+    new Racket(playingField.width, false, playingField.height, opponentInput.asLocal)
+//    new Racket(playingField.width, false, playingField.height, Signal {
+//      remote[Client].connected() match {
+//        case opponent :: _ => (opponentInput from opponent).asLocal()
+//        case _ => 0
+//      }
+//    })
   }
   placed[Server].local { implicit! => shapes.transform(racket2.shape :: _) }
 
   placed[Server].local { implicit! =>
     def makeBall(initVx: Double, initVy: Double) = {
-      val bouncingBall = new BouncingBall(initVx, initVy, Var(50), panel.Mouse.middleButton.pressed)
+      val bouncingBall = new BouncingBall(initVx, initVy, Var(50), panelX.Mouse.middleButton.pressed)
       shapes.transform(bouncingBall.shape :: _)
 
       val fieldCollisions = playingField.colliders(bouncingBall.shape)
@@ -88,14 +86,14 @@ object QNetworkRacketBall {
     makeBall(-200d, 100d)
 
     new Main {
-      override val panel: ShapesPanel = panel
+      override val panel: ShapesPanel = QNetworkRacketBall.panelX
     }.main(Array())
   }
 }
 
 object GameServer extends App {
   retier.multitier setup new QNetworkRacketBall.Server {
-    def connect = TCP(1099)
+    def connect = listen[QNetworkRacketBall.Client] { TCP(1099) }
   }
 }
 
